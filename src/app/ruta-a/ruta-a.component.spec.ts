df57f49bd39a9e2154488ee7e7a8476a050b8a47
Diego Herrera <vermicida@gmail.com>
2017-10-05 23:32:25 +0200

    Añadimos un test de integración para validar la comunicación entre componente y servicio.

--- src/app/ruta-a/ruta-a.component.spec.ts
+++ src/app/ruta-a/ruta-a.component.spec.ts
@@ -1,5 +1,10 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+// En 'rxjs/add/observable/x' están las funciones estáticas de Observable.
+// En 'rxjs/add/operator/x' están los operadores Observable.
+import 'rxjs/add/observable/from';
 
 import { RutaAComponent } from './ruta-a.component';
 import { ListaContactosComponent } from '../lista-contactos/lista-contactos.component';
@@ -38,7 +43,32 @@ describe('RutaAComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // Cuando necesitemos hacer inyección de dependencias, nos apoyamos en la función 'inject'. En ella indicamos
+  // como primer parámetro la colección de servicios a inyectar. Como segundo parámetro, indicamos una función
+  // anónima con tantos parámetros como servicios a inyectar indicamos: en estos parámetros nos informarán las
+  // instancias inyectadas.
+  it('debería obtener los contactos del servidor', inject([ContactosService], (contactosService: ContactosService) => {
+
+    const datosMockeados = [{
+      id: 1,
+      nombre: 'Bob',
+      apellidos: 'Esponja'
+    }];
+
+    // Con 'spyOn' observamos una función de un objeto dado. Seguidamente indicamos
+    // qué acción realizar cuando esta función observada sea ejecutada.
+    spyOn(contactosService, 'obtenerContactos').and.callFake(() => {
+      return Observable.from([datosMockeados]);
+    });
+
+    component.ngOnInit();
+
+    component.contactos$.subscribe((datos) => {
+      // Como debemos comprobar que los datos que retorna el servicio son los
+      // obtenidos en el componente, nos da igual la 'forma' que tengan: tan solo
+      // nos interesa que sean justo los mismos.
+      expect(datos).toBe(datosMockeados);
+    });
+
+  }));
 });
